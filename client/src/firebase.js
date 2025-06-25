07d0d5f5991a444d627d3a051ea9155ca9e02795
code.chivalric <code.chivalric@gmail.com>
2025-06-25 23:47:00 +0000

    Ran firebase init

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,17 +2,21 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+// import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-   apiKey: "AIzaSyAuKJnKt7mm3jhIGVndV6WUdOL-MJ4egOE",
-  authDomain: "food-service-app-slc.firebaseapp.com",
-  projectId: "food-service-app-slc",
-  storageBucket: "food-service-app-slc.firebasestorage.app",
-  messagingSenderId: "196570350035",
-  appId: "1:196570350035:web:3d545b683c1bb03750af26"
+  apiKey: "AIzaSyBdhu2-fURBmtPbX7mzeGpuzMfvVMAJddI",
+  authDomain: "food-service-app-slc-dev.firebaseapp.com",
+  databaseURL: "https://food-service-app-slc-dev-default-rtdb.firebaseio.com",
+  projectId: "food-service-app-slc-dev",
+  storageBucket: "food-service-app-slc-dev.firebasestorage.app",
+  messagingSenderId: "627559184124",
+  appId: "1:627559184124:web:70e8d14a8f670862028979",
+  measurementId: "G-KW399PS9HX"
 };
 
 const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 export { db, auth };
