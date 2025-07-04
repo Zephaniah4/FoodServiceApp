c4b8672e1a7f2529738edcef890c39bb80e5bb38
code.chivalric <code.chivalric@gmail.com>
2025-07-04 17:16:39 +0000

    Added a .env file

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -2,21 +2,22 @@
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
-// import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBdhu2-fURBmtPbX7mzeGpuzMfvVMAJddI",
-  authDomain: "food-service-app-slc-dev.firebaseapp.com",
-  databaseURL: "https://food-service-app-slc-dev-default-rtdb.firebaseio.com",
-  projectId: "food-service-app-slc-dev",
-  storageBucket: "food-service-app-slc-dev.firebasestorage.app",
-  messagingSenderId: "627559184124",
-  appId: "1:627559184124:web:70e8d14a8f670862028979",
-  measurementId: "G-KW399PS9HX"
+  // It is recommended to store sensitive information like API keys in environment variables
+  // instead of hardcoding them directly in the code for security reasons.
+  // Access environment variables using process.env.YOUR_VARIABLE_NAME
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
 const db = getFirestore(app);
 const auth = getAuth(app);
 export { db, auth };
