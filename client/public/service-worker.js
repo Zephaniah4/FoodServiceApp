ecc2245d5af0131701357396d6f3e6a3a2204669
Zephaniah4 <ojzhinckson@gmail.com>
2025-08-20 22:50:59 -0500

    new file:   client/public/app.png 	new file:   client/public/logo1.png 	new file:   client/public/logo2.png 	modified:   client/public/manifest.json 	new file:   client/public/service-worker.js 	modified:   client/src/index.js

---
+++ client/public/service-worker.js
@@ -0,0 +1,18 @@
+const CACHE_NAME = "food-service-cache-v1";
+const urlsToCache = ["/", "/index.html", "/static/js/main.6dca0028.js"];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
